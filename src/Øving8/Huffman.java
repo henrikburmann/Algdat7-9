f0f661fd2893a06e9e03ab5704a34fdb2ca22743
Henrik Burmann <henriabu@stud.ntnu.no>
2021-11-04 11:36:14 +0100

    String-variant av bits introdusert

--- src/Øving8/Huffman.java
+++ src/Øving8/Huffman.java
@@ -1,21 +1,62 @@
 package Øving8;
 
-import java.io.*;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Huffman {
-    private static int arraySize = 256;
+    private static int ARRAY_SIZE = 256;
+    HuffmanNode root;
+    String[] bitStrings;
 
     public Huffman(String text){
-        int freqency[] = frequencyTable(text);
+        root = huffmanTree(frequencyTable(text));
+        bitStrings = new String[ARRAY_SIZE];
+        lookUpTable(root, "");
     }
 
-    public static int[] frequencyTable(String text){
-        int[] freq = new int[arraySize];
+    public int[] frequencyTable(String text){
+        int[] freq = new int[ARRAY_SIZE];
         for (char character: text.toCharArray()){
             freq[character]++;
         }
         return freq;
+    }
+
+    public HuffmanNode huffmanTree(int[] freq){
+        PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<>();
+        for (char i = 0; i < ARRAY_SIZE; i++) {
+            if (freq[i] > 0){
+                priorityQueue.add(new HuffmanNode(i, freq[i], null, null));
+            }
+        }
+        if (priorityQueue.size() == 1){
+            priorityQueue.add(new HuffmanNode('z',1, null, null));
+        }
+        while (priorityQueue.size() > 1){
+            HuffmanNode left = priorityQueue.poll();
+            System.out.println(left.character + " " +left.frequency);
+            HuffmanNode right = priorityQueue.poll();
+            System.out.println(right.character + " " + right.frequency);
+            HuffmanNode parent = new HuffmanNode('\0',
+                    left.frequency + right.frequency,
+                    left, right);
+            priorityQueue.add(parent);
+        }
+        return priorityQueue.poll();
+    }
+
+    public void lookUpTable(HuffmanNode n, String s){
+        if (n.isLeaf()){
+            bitStrings[n.character] = s;
+            System.out.println(n.character + bitStrings[n.character]);
+            return;
+        }
+        lookUpTable(n.leftChild,  s + "0");
+        lookUpTable(n.rightChild, s + "1");
+    }
+
+    public String getLookUp(){
+        return bitStrings[107];
     }
 
 }
--- src/Øving8/Main.java
+++ src/Øving8/Main.java
@@ -1,12 +1,24 @@
 package Øving8;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String test = "Heiii";
+        String test = "alle barna elsker selskap";
         Huffman h = new Huffman(test);
-        
+
 
     }
+    public static void skrivUt(HuffmanNode n){
+        if (n.isLeaf()){
+            System.out.println(n.character + " " + n.frequency);
+            return;
+        }
+        skrivUt(n.leftChild);
+        skrivUt(n.rightChild);
+    }
+
+
 }
