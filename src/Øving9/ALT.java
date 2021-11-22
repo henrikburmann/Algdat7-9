d7e99245159fe9e86fffded2c4871dcc52cb5856
Marcus Johannessen <marcusjo@stud.ntnu.no>
2021-11-22 19:17:55 +0100

    little clean

--- src/Øving9/ALT.java
+++ src/Øving9/ALT.java
@@ -32,19 +32,10 @@ public class ALT {
         priorityQueue = new PriorityQueue<>(nodes.size(), new DistanceComprator());
     }
 
-    public ArrayList<int[]> getToLandmarks() {
-        return toLandmarks;
-    }
-
-    public ArrayList<int[]> getFromLandmarks() {
-        return fromLandmarks;
-    }
-
     public void readFile(String nodeFile, String edgeFile, String inpktFile) throws IOException {
         // Leser noder
         BufferedReader brNodes = new BufferedReader(new FileReader(nodeFile));
         StringTokenizer stNodes = new StringTokenizer(brNodes.readLine());
-
         int size = Integer.parseInt(stNodes.nextToken());
 
         for (int i = 0; i < size; i++) {
@@ -55,10 +46,11 @@ public class ALT {
             nodes.add(new Node(number, longitude, latitude));
         }
 
-        //Leser kanter
+        // Leser kanter
         BufferedReader brEdges = new BufferedReader(new FileReader(edgeFile));
         StringTokenizer stEdges = new StringTokenizer(brEdges.readLine());
         size = Integer.parseInt(stEdges.nextToken());
+
         for (int i = 0; i < size; i++) {
             stEdges = new StringTokenizer(brEdges.readLine());
             Node from = nodes.get(Integer.parseInt(stEdges.nextToken()));
@@ -67,7 +59,7 @@ public class ALT {
             edges.add(new Edge(from, to, weight));
         }
 
-        // leser interessepkt
+        // Leser interessepkt
         BufferedReader brInteressepkt = new BufferedReader(new FileReader(inpktFile));
         StringTokenizer stInteressepkt = new StringTokenizer(brInteressepkt.readLine());
         int intressepktSize = Integer.parseInt(stInteressepkt.nextToken());
@@ -84,9 +76,9 @@ public class ALT {
 
         }
 
+        // legger til nabo nodene til hver node
         addNeigbours();
         addOppoNeigbours();
-        //  readLandmarkDistances();
     }
 
     /**
@@ -94,16 +86,14 @@ public class ALT {
      */
     public void readFromLandmarks() throws IOException {
         BufferedReader bfToNode = new BufferedReader(new FileReader("src/Øving9/Files/outfiles/from_landmark_to_node.txt"));
-        StringTokenizer stToNode = null;
+        StringTokenizer stToNode;
         for (int i = 0; i < 4; i++) {
             readFromLandmarks.add(new int[nodes.size()]);
         }
         for (int i = 0; i < nodes.size(); i++) {
-            // må finne en måte å se om man kan lese neste linje
             stToNode = new StringTokenizer(bfToNode.readLine());
 
-            for (int j = 0; j < 4; j++) {
-
+            for (int j = 0; j < 4; j++) { // 4 landemerker, litt hardkodet
                 if (!stToNode.hasMoreTokens()) {
                     break;
                 }
@@ -126,11 +116,11 @@ public class ALT {
     public void readToLandmarks() throws IOException {
         BufferedReader bfToNode = new BufferedReader(new FileReader("src/Øving9/Files/outfiles/from_node_to_landmarks.txt"));
         StringTokenizer stToNode = null;
-        for (int i = 0; i < 4; i++) {
+
+        for (int i = 0; i < 4; i++) { // 4 landemerker, litt hardkodet
             readtoLandmarks.add(new int[nodes.size()]);
         }
         for (int i = 0; i < nodes.size(); i++) {
-            // ikke riktig størrelse
             String next = bfToNode.readLine().trim();
             if (next.isEmpty()) {
                 bfToNode.close();
@@ -148,18 +138,6 @@ public class ALT {
             s += readtoLandmarks.get(i).length;
         }
         System.out.println("Antall ting og sånn er til landemerkene: " + s);
-    }
-
-
-    // liten test bare for å se at den lagrer distansene i fromLandmark eller toLandmark
-    public void printLandmarks(int landmark, ArrayList<int[]> list) {
-        if (landmark < 0 || landmark > 3) {
-            landmark = 0;
-        }
-        int[] li = list.get(landmark);
-        for (int i = 0; i < li.length; i++) {
-            System.out.println(li[landmark]);
-        }
     }
 
     /**
@@ -189,7 +167,7 @@ public class ALT {
         }
     }
 
-    // for å hente ut spesifik node fra en index i nodes
+    // for å hente ut spesifik node fra en index i nodes liste
     public Node getNodeFromList(int index) {
         return nodes.get(index);
     }
@@ -199,8 +177,8 @@ public class ALT {
     }
 
     /**
-     * Lager en fil med distansen fra startnodene til
-     * alle landemerkene som man har definert
+     * Lager en fil med distansen fra startnodene
+     * til alle landemerkene
      */
     public void generateFromNodeToLandmarkFile(int n, int s, int e, int w) throws IOException {
         FileWriter fileWriter = new FileWriter("src/Øving9/Files/outfiles/from_node_to_landmarks.txt");
@@ -217,7 +195,6 @@ public class ALT {
             int[] distances = findShortestDistanceFromAll(landmarks[i]);
             toLandmarks.add(distances);
         }
-
         for (int j = 0; j < nodes.size(); j++) {
             pw.write(toLandmarks.get(0)[j] + " "
                     + toLandmarks.get(1)[j] + " "
@@ -230,6 +207,8 @@ public class ALT {
 
     /**
      * @throws FileNotFoundException
+     * Lager en fil med distansen til startnode
+     * fra alle landemerkene
      */
     public void generateToNodeFromLandmarkFile(int n, int s, int e, int w) throws IOException {
         FileWriter outFile = new FileWriter("src/Øving9/Files/outfiles/from_landmark_to_node.txt");
@@ -248,7 +227,6 @@ public class ALT {
             int[] distances = findShortestDistanceToAll(landmarks[i]);
             fromLandmarks.add(distances);
         }
-
         for (int j = 0; j < nodes.size(); j++) {
             pw.write(fromLandmarks.get(0)[j] + " "
                     + fromLandmarks.get(1)[j] + " "
@@ -318,8 +296,8 @@ public class ALT {
 
     /**
      * @param start method from Dijkstra
-     *              Tar inn en startnode og finner avstand
-     *              fra node til start
+     * Tar inn en startnode og finner avstand
+     * fra node til start
      */
     private int[] findShortestDistanceFromAll(Node start) {
         reset();
@@ -362,7 +340,7 @@ public class ALT {
     }
 
     //
-    public int landmarkEstimate(int from, int to, int landmark) {
+    public int estimateLandmark(int from, int to, int landmark) {
         int landmarkToCurrent = readFromLandmarks.get(landmark)[from];
         int landmarkToTarget = readFromLandmarks.get(landmark)[to];
 
@@ -383,7 +361,7 @@ public class ALT {
         int length = 4; // hardkodet, avhengig av hvor mye landemerker som brukes
 
         for (int i = 0; i < length; i++) {
-            tempEstimate = landmarkEstimate(from, to, i); // landmark
+            tempEstimate = estimateLandmark(from, to, i); // landmark
             if (tempEstimate > estimate) {
                 estimate = tempEstimate;
             }
@@ -391,14 +369,13 @@ public class ALT {
         return estimate;
     }
 
-
     /**
      * @param startNode
      * @param endNode
      * @return distance from startnode to endnode
      * ALT search (djikstra with landmarks)
      */
-    public int search(int startNode, int endNode) {
+    public int altSearch(int startNode, int endNode) {
         priorityQueue.clear();
         reset();
         amountVisited = 0;
@@ -413,11 +390,6 @@ public class ALT {
             amountVisited++;
             polled.setVisisted(true);
             polled.setEnQueued(false);
-            /*
-            if (polled.equals(endNode)) {
-                return; // we have reached the end
-            }
-             */
 
             for (Edge edge : polled.getAdjList()) {
                 Node toNode = edge.getTo();
@@ -437,14 +409,11 @@ public class ALT {
                         toNode.setEnQueued(true);
                     }
                 }
-
                 if (!toNode.isVisisted() && !toNode.isEnQueued()) {
                     priorityQueue.add(toNode);
                     toNode.setEnQueued(true);
                 }
-
             }
-
             if (end.isVisisted()) {
                 System.out.println("Antall besøkte: " + amountVisited);
                 break;
@@ -481,7 +450,7 @@ public class ALT {
         }
     }
 
-    // henter ut interessepkt basert på nodenummer
+    // henter ut interessepktet basert på nodenummer, (kan ikke bruke index..)
     public Interessepkt getInteressepkt(int number) {
         for (int i = 0; i < interessepkts.size() ; i++) {
             if (interessepkts.get(i).getNodeNumber() == number) {
--- src/Øving9/Client.java
+++ src/Øving9/Client.java
@@ -4,6 +4,18 @@ import java.io.IOException;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+
+        String nodePath = "src/Øving9/Files/Oppg/noder.txt";
+        String edgePath = "src/Øving9/Files/Oppg/kanter.txt";
+        String intressepktPath = "src/Øving9/Files/Oppg/interessepkt.txt";
+
+        // Kjører ALT algoritme
+        ALT alt = new ALT(nodePath, edgePath, intressepktPath);
+
+        // preprocess filer, trenger ikke å kjøre disse hver gang
+        //alt.generateFromNodeToLandmarkFile(5263302,2313313, 708400, 5486883);
+        //alt.generateToNodeFromLandmarkFile(5263302,2313313, 708400, 5486883);
+
         int fra = 6861306; // Trondheim
         //int fra = 3447384; // Stavanger
         //int fra = 5379848; // Snåsa
@@ -12,72 +24,41 @@ public class Client {
         //int til = 136963; // Tampere
         //int til = 2951840; // Mehamn
 
-        //Kjører først ALT-metoden
-        ALT alt = new ALT(
-                "src/Øving9/Files/Oppg/noder.txt",
-                "src/Øving9/Files/Oppg/kanter.txt",
-                "src/Øving9/Files/Oppg/interessepkt.txt");
-
+        // leser av preprosecced
         alt.readFromLandmarks();
         alt.readToLandmarks();
 
+        // printer svar for ALT
         printAnswer(fra, til, alt);
 
 
-
-
-        //alt.generateFromNodeToLandmarkFile(5263302,2313313, 708400, 5486883);
-        //alt.generateToNodeFromLandmarkFile(5263302,2313313, 708400, 5486883);
-
-
-        //
-        //System.out.println("Beregnet tid er: " + omregnTilTimer(alt.search(fra, til)) + "\n\n");
-        //System.out.println("Koordinater: \n");
-        //Node n = alt.getNodeFromList(til);
-/*
-        List<String> koordinater = alt.getPath(n);
-        for (int i = 0; i < koordinater.size(); i++) {
-            System.out.println(koordinater.get(i));
-        }
-
- */
-        //System.out.println("Fasit er: 17 timer, 39 minutter, 43 sekunder");
-        }
-
-
-        /*File nodeEksempel = new File("src/Øving9/Files/Oppg/noder.txt");
-        File edgeEksempel = new File("src/Øving9/Files/Oppg/kanter.txt");
-
-        //Kjører så Djikstra
-        Djikstra d = new Djikstra(nodeEksempel.toString(), edgeEksempel.toString() );
-
-        // Tar utgangspunkt i en node og regner ut alle korteste distanser fra denne til de andre nodene
+        //Kjører så Djikstra algoritme
+        Djikstra d = new Djikstra(nodePath, edgePath);
         d.findShortestDistance(d.getNodeFromList(fra), d.getNodeFromList(til));
         // Printer ut distansen fra startnoden øverst
 
-        System.out.println("\nDistanse fra node " + fra + " til " + til  + " = " + d.getNodeFromList(til).getDistance());
-        System.out.println("Korteste vei fra Trondheim til Oslo: " + d.getShortestPath(d.getNodeFromList(til)));*/
+        System.out.println("\nDistanse fra node " + fra + " til " + til + " = " + d.getNodeFromList(til).getDistance());
+
+        System.out.println("Korteste vei fra Trondheim til Oslo: " + d.getShortestPath(d.getNodeFromList(til)));
+
+        // printer ut kortest path i form av node nummer.
+        System.out.println(d.getShortestPath(d.getNodeFromList(til)));
+
+    }
 
 
-
-        // Djikstra d = new Djikstra(nodeEksempel.toString(), edgeEksempel.toString() );
-    //d.findShortestDistanceToAll(d.getNodeFromList(0));
-    // printer ut path fra startnoden som man har definert øverst
-    // System.out.println(d.getShortestPath(d.getNodeFromList(til)));
-    //d.generateToStartFromLandmarkFile(5263302,2313313, 4102960, 1089301);
-    //}
-
-    // ALT alt = new ALT(nodeEksempel.toString(), edgeEksempel.toString());
-
+    // printer ut svar for ALT på riktig format
     static void printAnswer(int start, int end, ALT alt) {
         Interessepkt startIntresse = alt.getInteressepkt(start);
         Interessepkt endInteresse = alt.getInteressepkt(end);
 
-        int tid = alt.search(start, end);
+        int tid = alt.altSearch(start, end);
         String time = omregnTilTimer((long) tid);
 
         System.out.println("tur: " + startIntresse.getName() + "-" + endInteresse.getName()
-                + ", startnode " + start + ", endnode " + end + ", Antall besøkte: " + alt.getAmountVisited() +  ", " + time);
+                + ", startnode " + start + ", endnode " + end + ", Antall besøkte: " + alt.getAmountVisited() + ", " + time);
+
+        // printer ut koordinatene til ruten
         /*
         System.out.println("Rutens koordinater: \n");
         List<String> koordinater = alt.getPath(alt.getNodeFromList(end));
@@ -89,15 +70,15 @@ public class Client {
 
     }
 
-
-    public static int omregnTilHundredel(int timer, int minutter, int sekunder){
+    public static int omregnTilHundredel(int timer, int minutter, int sekunder) {
         int svar = 0;
         svar += sekunder * 100;
         svar += minutter * 60 * 100;
         svar += timer * 60 * 100 * 60;
         return svar;
     }
-    public static String omregnTilTimer(long hundredeler){
+
+    public static String omregnTilTimer(long hundredeler) {
         int timer = (int) hundredeler / (100 * 60 * 60);
         int restTimer = (int) hundredeler % (100 * 60 * 60);
 
@@ -107,5 +88,4 @@ public class Client {
         int sekunder = restMinutter / 100;
         return "Timer: " + timer + ". Minutter: " + minutter + ". Sekunder: " + sekunder;
     }
-
 }
